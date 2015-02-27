5d0c02274b0ee8ef0837e7f28e1dd8320c7bb006
fenris85 <fenris85@gmail.com>
2015-02-27 14:39:43 +0100

    mobile & desktop layout changes

---
+++ summit/static/js/misc.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function(){
+
+    /* Mobile sidebar menu */
+
+    var opened = false;
+
+    $('#js-menu-button').bind('click', function(){
+
+        if( opened ) {
+            $('.sidebar-nav').removeClass('sidebar-nav--opened');
+            opened = false;
+        }else{
+            $('.sidebar-nav').addClass('sidebar-nav--opened');
+            opened = true;
+        }
+    });
+
+    $( document ).on( "click", ".container", function() {
+        $('.sidebar-nav').removeClass('sidebar-nav--opened');
+        opened = false;
+    });
+
+
+
+    $('.navigation__link, .sidebar__menu__link').bind('click', function(e) {
+        e.preventDefault();
+        if( $(this).data('target') !== undefined) {
+            $('.sidebar-nav').removeClass('sidebar-nav--opened');
+            opened = false;
+            $('body').scrollTo('.' + $(this).data('target'), 1000, {offset: -40});
+        }
+    });
+});
