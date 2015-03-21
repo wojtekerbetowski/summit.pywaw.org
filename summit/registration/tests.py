854b9d0e6ddfdaeacbc0bd7535957f6032362bf1
konradhalas <halas.konrad@gmail.com>
2015-03-21 20:17:36 +0100

    Send email after registration.

--- summit/registration/tests.py
+++ summit/registration/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from djet.assertions import EmailAssertionsMixin, StatusCodeAssertionsMixin
+from djet.testcases import ViewTestCase
+from . import views
 
-# Create your tests here.
+
+class AttendeeCreateViewTest(ViewTestCase, EmailAssertionsMixin, StatusCodeAssertionsMixin):
+    view_class = views.AttendeeCreateView
+
+    def test_post_should_create_attendee_and_send_payment_info(self):
+        data = {
+            'name': 'John Lennon',
+            'tagline': 'Guitar Master',
+            'email': 'john@lenon.com',
+            'twitter': '@johnlennon',
+            'phone': '123123123',
+            'location': 'London, UK',
+            'display_on_website': True,
+            'invoice': True,
+            'company_name': 'The Beatles',
+            'company_address': 'Main St, London, UK',
+            'company_nip': '123123123',
+            'accept_terms_of_service': True,
+        }
+        request = self.factory.post(data=data)
+
+        response = self.view(request)
+
+        self.assert_redirect(response, reverse('home'))
+        self.assert_emails_in_mailbox(1)
+        self.assert_email_exists(to=[data['email']])
--- summit/registration/views.py
+++ summit/registration/views.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse_lazy
+from django.template.loader import render_to_string
 from django.views.generic import CreateView
 from . import models, forms
 
@@ -8,3 +12,21 @@ class AttendeeCreateView(CreateView):
     form_class = forms.AttendeeForm
     template_name = 'registration.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self._send_payment_email()
+        return response
+
+    def _send_payment_email(self):
+        context = {
+            'attendee': self.object,
+            'site': get_current_site(self.request),
+        }
+        send_mail(
+            subject=render_to_string('emails/registration_subject.txt', context).strip(),
+            message=render_to_string('emails/registration_body.txt', context),
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[self.object.email],
+        )
+
