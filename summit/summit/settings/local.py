a756908927a47d555102c78805b94f3fb553e3db
konradhalas <halas.konrad@gmail.com>
2015-03-21 19:26:13 +0100

    Configure db.

--- summit/summit/settings/local.py
+++ summit/summit/settings/local.py
@@ -5,3 +5,10 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- summit/summit/settings/staging.py
+++ summit/summit/settings/staging.py
@@ -5,3 +5,14 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['dev.summit.pywaw.org']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_var('DB_NAME'),
+        'USER': get_env_var('DB_USER'),
+        'PASSWORD': get_env_var('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
